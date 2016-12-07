3a516ad69d11cf3dc693463385e4993398c8532b
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-12-07 20:24:37 +1100

    removed the empty line number

--- website/javascripts/script.js
+++ website/javascripts/script.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
   // putting lines by the pre blocks
   $("pre").each(function(){
     var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
+    var lines = new Array(pre.length);
+    for(var i = 0; i < pre.length - 1; i++) {
       var wrap = Math.floor(pre[i].split("").length / 70)
       if (pre[i]==""&&i==pre.length-1) {
         lines.splice(i, 1);
       } else {
         lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
+        for(var j = 0; j < wrap - 1; j++) {
           lines[i] += "\n";
         }
       }
