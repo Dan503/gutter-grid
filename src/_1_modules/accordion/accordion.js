a624c15b8ebf479495e875b11fe802d42a6d8fa1
Daniel Tonon <Dan503@users.noreply.github.com>
2017-01-15 17:52:34 +1100

    Accordion menu functionality added

---
+++ src/_1_modules/accordion/accordion.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+//npm imports
+import $ from 'jquery';
+
+//module imports
+//import { example } from 'example/example';
+
+const //hooks
+	_module =  $('.JS-accordion'),
+	_revealer = $('.JS-accordion__revealer'),
+	_toggle = $('.JS-accordion__toggle');
+
+const //classes
+	open_ = '-accordion--open';
+
+//module functionality
+class accordion {
+	constructor(elem){
+		const This = this;
+		this.elem = elem;
+		this.$elem = $(elem);
+		this.$toggle = this.$elem.find(_toggle);
+		this.$revealer = this.$elem.find(_revealer);
+
+		console.log(this.$toggle);
+
+		this.$toggle.click((e)=>{
+			e.preventDefault();
+			this.toggle();
+			})
+	}
+
+	//toggles accordion open and closed
+	toggle(){
+		this.$revealer.slideToggle();
+		this.$elem.toggleClass(open_);
+	}
+}
+
+//This function is called on page load unless the name of this file starts with an underscore
+export default function() {
+	_module.each(function(e){
+		new accordion(this);
+	})
+}
