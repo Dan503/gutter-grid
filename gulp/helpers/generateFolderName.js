c90086d7a62fc138521e9b7318202bd89414a865
Daniel Tonon <Dan503@users.noreply.github.com>
2017-01-22 17:57:28 +1100

    Generated pages now adds page index to start of folder name
    This helps order the pages in the same order that they appear in the nav to make them easier to find

--- gulp/helpers/generateFolderName.js
+++ gulp/helpers/generateFolderName.js
@@ -1,7 +1,10 @@
-export default function generateFolderName(pageTitle){
+export default function generateFolderName(index, pageTitle){
+	//add leading 0 to indexes less than 10
+	index = index < 10 ? `0${index}` : index;
+
 	//convert to lowercase
 	//then remove all quote characters, both single and double
 	//then replace all characters that are not a letter or a number with a "-" dash
 	//then in areas that have multiple dashes next to one another, replace them with a single dash
-	return pageTitle.toLowerCase().replace(/['|"]/g, "").replace(/[^a-z0-9]/g, "-").replace(/-+/g, '-');
+	return [index,'-',pageTitle.toLowerCase().replace(/['|"]/g, "").replace(/[^a-z0-9]/g, "-").replace(/-+/g, '-')].join('');
 }
