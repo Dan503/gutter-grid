e4302d2e000c10f5a731e4f9695ced8e3c49ae15
Daniel Tonon <Dan503@users.noreply.github.com>
2017-01-28 15:47:20 +1100

    getting rid of IE8 pubSub stuff, can't get it to work

--- src/_0_scripts/ie8-main.js
+++ src/_0_scripts/ie8-main.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
 
-//alternative pubSub script
-//https://davidwalsh.name/pubsub-javascript
-var events = (function(){
-	var topics = {};
-	var hOP = topics.hasOwnProperty;
-
-	return {
-		subscribe: function(topic, listener) {
-			// Create the topic's object if not yet created
-			if(!hOP.call(topics, topic)) topics[topic] = [];
-
-		},
-		publish: function(topic, info) {
-			// If the topic doesn't exist, or there's no listeners in queue, just leave
-			if(!hOP.call(topics, topic)) return;
-
-			//fire!
-			return topics[topic](info !== undefined ? info : {});
-		}
-	};
-})();
-
-
-
 //IE8 tabs code
 	var //hooks
 	_module = $('.JS-tabs'),
@@ -42,7 +18,6 @@ var events = (function(){
 		this.$elem = $(elem);
 		this.$triggers = this.$elem.find('.JS-tabs__trigger');
 		this.$content = this.$elem.find('.JS-tabs__content');
-		this.is_defaultSwitcher = this.$elem.is('#JS-tabs__defaultSelector');
 
 		this.$triggers.click(function (e) {
 			e.preventDefault();
@@ -55,12 +30,6 @@ var events = (function(){
 			switchTab(0);
 		}
 
-		if (!this.is_defaultSwitcher) {
-			events.subscribe('defaultSwitch', function (index) {
-				switchTab(index);
-			});
-		}
-
 		function switchTab(tab) {
 
 			if ($.isNumeric(tab)) {
@@ -69,18 +38,12 @@ var events = (function(){
 
 			var pos = tab.parent().index();
 
-			console.log(This.$triggers.filter('.' + active_).length);
-
 			This.$triggers.filter('.' + active_).removeClass(active_);
 			tab.addClass(active_);
 
 			This.$content.hide();
 			This.$content.eq(pos).show();
 
-			if (This.is_defaultSwitcher) {
-				window.localStorage.activeTab = pos;
-				events.publish('defaultSwitch', pos);
-			}
 		}
 	}
 
