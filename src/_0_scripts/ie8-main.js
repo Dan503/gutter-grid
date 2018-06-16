88028533ca9a63fed4fcda2f7724c1af1155f342
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-06-16 16:21:26 +1000

    Fixed IE8 js error
    I thought indexOf would be safe to use but apparantly it isn't.
    I'm adding a polyfill to make it safe

--- src/_0_scripts/ie8-main.js
+++ src/_0_scripts/ie8-main.js
@@ -1,3 +1,10 @@
+// indexOf polyfill
+// Minified version of this:
+// https://stackoverflow.com/a/35054662/1611058
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf=function(r){var t=this.length>0,i=Number(arguments[1])||0;for((i=i<0?Math.ceil(i):Math.floor(i))<0&&(i+=t);i<t;i++)if(i in this&&this[i]===r)return i;return-1};
+}
+
 $(document).ready(function(){
 
 //IE8 tabs code
